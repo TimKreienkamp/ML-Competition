60debe9f63e758c9633f119e9cb9f76f1a047bef
TimKreienkamp <tim.kreienkamp@gmail.com>
2015-03-10 13:32:50 +0100

    reducing number of trees to 80

--- Code/test_rf_for_feature_eng.R
+++ Code/test_rf_for_feature_eng.R
@@ -10,10 +10,15 @@ for (i in 1:10){
   data[,i] <- as.numeric(data[,i])
 }
 
-#encode binary variables explicitly as factors
-for (i in 11:55){
+#encode binary variables explicitly as factors - this step can be skipped
+for (i in 11:54){
   data[,i] <- as.factor(data[,i])
 }
+
+data[,55] <- as.factor(data[,55])
+
+data$EDTH <- data$elevation - data$hor_dist_hyd*0.2
+data$EVTH <-  data$elevation - data$ver_dist_hyd
 
 ###### HERE GOES THE FEATURE ENGINEERING PART##########
 
@@ -25,10 +30,10 @@ covertype.hex <- as.h2o(localH2o,
                         key = "covertype.hex")
 
 # train and cross validate random forest
-rf <- h2o.randomForest(x = 1:54, #column indices of features
+rf <- h2o.randomForest(x = c(1:54, 56, 57), #column indices of features
                        y = 55, # column index of response
-                       covertype.hex, nfolds = 10, 
-                       ntree = 225) 
+                       covertype.hex, nfolds = 10, sample.rate = 0.9,
+                       ntree = 80) 
 
 # look at the overall cross validation error and see if it is better than 0.14
 rf
